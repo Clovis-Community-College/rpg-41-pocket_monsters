ab98d9dc072b2cde874379b96bd250937e878bbe
logancg14 <logancg14@gmail.com>
2026-04-11 21:18:46 -0700

    gave Bjarne a shotgun

--- class_slop.h
+++ class_slop.h
@@ -10,6 +10,7 @@ class slop {//Actor class but my names are better
 		//the current position of the hero/monster
 		int x = 0;
 		int y = 0;
+		int movement = 0; //this is how many spaces each class can move within the battlefield
 	public:
 		string get_name() const {
 			return name;
@@ -38,8 +39,14 @@ class slop {//Actor class but my names are better
 		int get_y() const {
 			return y;
 		}
-		int set_y(int temp) {
+		void set_y(int temp) {
 			y = temp;
+		}
+		int get_movement() const {
+			return movement;
+		}
+		void set_movement(int temp) {
+			movement = temp;
 		}
 };
 
@@ -50,6 +57,7 @@ class move {
 		int dmg = 0;//for damage if it does damage it is negative if it heals its positive and 0 is for moves that deal no damage
 		bool special_effect = false;
 		bool area_of_effect = false; //if this is true it means it can attack/heal everyone on one side of the battlefield
+		int range = 0;
 	public:
 		enum STATUS {
 			BUFF_ATTK,
@@ -82,7 +90,7 @@ class move {
 		bool has_special_effect() const {
 			return special_effect;
 		}
-		void set_special_effect(bool temp) {
+		void is_special_effect(bool temp) {
 			special_effect = temp;
 		}
 		bool is_area_of_effect() const {
@@ -91,12 +99,27 @@ class move {
 		void set_area_of_effect(bool temp) {
 			area_of_effect = temp;
 		}
+		int get_range() const {
+			return range;
+		}
+		void set_range(int temp) {
+			range = temp;
+		}
+		void set_special_effect(STATUS temp) {
+			sp_effect = temp;
+		}
+		STATUS get_special_effect() const {
+			return sp_effect;
+		}
+
+		protected:
+		STATUS sp_effect;
 };
 
 class Software_Engineers : public slop, public move {//Heroes class, again my naming scheme is better
 	protected:
 		vector<move> moves{3};
-		STATUS sp_effect;
+		//STATUS sp_effect;
 	public:
 		void add_move(move &temp, int move_index) {
 			moves.at(move_index) = temp;
@@ -104,34 +127,68 @@ class Software_Engineers : public slop, public move {//Heroes class, again my na
 		move get_move(int move_index) const {
 			return moves.at(move_index);
 		}
-		int apply_special_effect(STATUS temp){
+		float apply_special_effect(STATUS temp){
 			switch (temp) {
 				case BUFF_ATTK:
 					return 2;
-				case   
+				case BUFF_INSTINCT:
+					return 2;
+				case OMNI_BUFF:
+					return 1;
+				case DEBUFF_ATTK:
+					return -2;
+				case DEBUFF_INSTINCT:
+					return -2;
+				case OMNI_DEBUFF:
+					return -1;
+				case TOXIC:
+					return -0.5;
+				case HEALING_OVERTIME:
+					return 0.5;
 			}
 		}
 };
 
 class Kieth: public Software_Engineers {
-	Kieth () {
+	Kieth() {
 		name = "Kieth";
 		hp = 20;
 		instinct = 5;
 		moves.at(0).set_move_name("Make Mod");
 		moves.at(0).set_description("Elevates ally to Mod status boosting their stats");
 		moves.at(0).set_dmg(0);
-		moves.at(0).set_special_effect(true);
+		moves.at(0).is_special_effect(true);
+		moves.at(0).set_special_effect(OMNI_BUFF);
 		moves.at(0).set_area_of_effect(true);
+		moves.at(0).set_range(3);
 		moves.at(1).set_move_name("Debug");
 		moves.at(1).set_description("Removes all bugs in code healing allies");
 		moves.at(1).set_dmg(-2);
-		moves.at(1).set_special_effect(false);
+		moves.at(1).is_special_effect(false);
 		moves.at(1).set_area_of_effect(false);
+		moves.at(1).set_range(2);
+		moves.at(3).set_move_name("SegFault");
+		moves.at(3).set_description("Realeases toxins that makes the enemeies segfault irl causing them to grow weaker");
+		moves.at(3).set_dmg(0);
+		moves.at(3).is_special_effect(true);
+		moves.at(3).set_special_effect(DEBUFF_ATTK);
+		moves.at(3).set_area_of_effect(false);
+		moves.at(3).set_range(4);
 		//moves.at(2).set_move_name("
 	}
 };
-class Bjarne: public Software_Engineers {
+class Bjarne: public Software_Engineers {//maybe make status its own class
+	Bjarne() {
+		name = "Bjarne";
+		hp = 30;
+		instinct = 3;
+		moves.at(0).set_move_name("Shotgun");
+		moves.at(0).set_description("shotgun");
+		moves.at(0).set_dmg(6);
+		moves.at(0).is_special_effect(false);
+		moves.at(0).set_area_of_effect(false);
+		moves.at(0).set_range(1);
+	}
 };
 class Guava: public Software_Engineers {
 };
--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include "/public/read.h" // IWYU pragma: keep
 #include <vector>         // IWYU pragma: keep
 #include "class_slop.h"
+#include "inventory.h"
 using namespace std;
 
 int main() {
