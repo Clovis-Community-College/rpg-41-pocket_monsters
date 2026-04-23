c43d4b7734e8db22a10336941ebf94ef750bb05d
rangel_1018218 <1018218@my.scccd.edu>
2026-04-23 13:57:06 -0700

    changed most pointers to shared pointers and added a visualiser function

--- bridge_pain.h
+++ bridge_pain.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "/public/read.h" // IWYU pragma: keep
+#include <memory>
 #include <thread>
 #include <vector>         // IWYU pragma: keep
 #include "Bridges.h"
@@ -9,10 +10,24 @@ using namespace std;
 using namespace bridges;
 using namespace datastructure;
 
-void quicksort(vector<int> &array, vector<slop*> &peeps, int count) {
+void visualizer(Bridges &b, CircDLelement<shared_ptr<slop>> *curNode){
+	CircDLelement<shared_ptr<slop>> *savNode = curNode;
+	curNode = curNode->getNext();
+	savNode->setColor("red");
+	savNode->getLinkVisualizer(curNode)->setColor("green");
+	for(; curNode != savNode; curNode = curNode->getNext()){
+		curNode->setColor("aliceblue");
+		curNode->getLinkVisualizer(curNode->getNext())->setColor("magenta");
+	}
+	b.setDataStructure(savNode);
+	b.visualize();
+	curNode = savNode;
+}
+
+void quicksort(vector<int> &array, vector<shared_ptr<slop>> &peeps, int count) {
 	if(count < 2) return;
 	int pivot = array.at(count - 1);
-	slop *pivotPeep = peeps.at(count - 1);
+	shared_ptr<slop> pivotPeep = peeps.at(count - 1);
 	int i = 0, j = count - 1;
 	while (j > i) {
 		while (array.at(i) < pivot) {
@@ -22,7 +37,7 @@ void quicksort(vector<int> &array, vector<slop*> &peeps, int count) {
 			j--;
 		}
 		if (j > i) {
-			slop *swapPeep = peeps.at(i);
+			shared_ptr<slop> swapPeep = peeps.at(i);
 			peeps.at(i) = peeps.at(j);
 			peeps.at(j) = swapPeep;
 			int swap = array.at(i);
@@ -38,15 +53,15 @@ void quicksort(vector<int> &array, vector<slop*> &peeps, int count) {
 	}
 	vector<int> splitArrayOne;
 	vector<int> splitArrayTwo;
-	vector<slop*> splitPeepsOne;
-	vector<slop*> splitPeepsTwo;
+	vector<shared_ptr<slop>> splitPeepsOne;
+	vector<shared_ptr<slop>> splitPeepsTwo;
 	for(int k = 0; k < array.size(); k++){
 		if(k < i){
 			splitArrayOne.push_back(array.at(k));
-			splitPeepsOne.push_back(peeps.at(k));
+			splitPeepsOne.emplace_back(peeps.at(k));
 		} else {
 			splitArrayTwo.push_back(array.at(k));
-			splitPeepsTwo.push_back(peeps.at(k));
+			splitPeepsTwo.emplace_back(peeps.at(k));
 		}
 	}
 	quicksort(splitArrayOne, splitPeepsOne, i);
@@ -68,27 +83,30 @@ void quicksort(vector<int> &array, vector<slop*> &peeps, int count) {
 
 
 class Sphere{//circular linked list and its info
-	vector<slop*> softwareFolks;			//hero list 
-	vector<slop*> possiblyEvilSoftwareFolks;//enemy list 
-	CircDLelement<slop*> *root;				//"first" element, just here for moral support
-	CircDLelement<slop*> *currentSloppy;		//iykyk
+	vector<shared_ptr<slop>> softwareFolks;			//hero list 
+	vector<shared_ptr<slop>> possiblyEvilSoftwareFolks;//enemy list 
+	CircDLelement<shared_ptr<slop>> *root;				//"first" element, just here for moral support
+	CircDLelement<shared_ptr<slop>> *currentSloppy;		//iykyk
+	Bridges bridges;//2, "falseP0tato" ,"1041388770783");
 	int size = 0;
 
 	public:
-	Sphere(vector<slop*> &theGoodGuys, vector<slop*> &theBadGuys){
+	Sphere(vector<shared_ptr<slop>> &theGoodGuys, vector<shared_ptr<slop>> &theBadGuys): softwareFolks(theGoodGuys), possiblyEvilSoftwareFolks(theBadGuys), bridges(1, "falseP0tato","1041388770783") {
 		vector<int> speeds;
-		vector<slop*> massiveGuyVec;
-		softwareFolks = theGoodGuys;
-		possiblyEvilSoftwareFolks = theBadGuys;
+		vector<shared_ptr<slop>> massiveGuyVec;
+		//softwareFolks = theGoodGuys;
+		//possiblyEvilSoftwareFolks = theBadGuys;
 		for(int i = 0; i < theGoodGuys.size(); i++){
 			int speed = (rand() % 20) + theGoodGuys.at(i)->get_instinct();
 			speeds.push_back(speed);
-			massiveGuyVec.push_back(theGoodGuys.at(i));
+			massiveGuyVec.emplace_back(theGoodGuys.at(i));
+			size++;
 		}
 		for(int i = 0; i < theBadGuys.size(); i++){
 			int speed = rand() % 20 + theBadGuys.at(i)->get_instinct();
 			speeds.push_back(speed);
 			massiveGuyVec.push_back(theBadGuys.at(i));
+			size++;
 		}
 		cout << "There are " << massiveGuyVec.size() << " creatures in combat\n";
 		if(!massiveGuyVec.size()) return;
@@ -96,13 +114,13 @@ class Sphere{//circular linked list and its info
 		for(int i = 0; i < massiveGuyVec.size(); i++){
 			cout << massiveGuyVec.at(i)->get_name() << " is moving at " << speeds.at(i) << " mph\n";
 			if(i == 0) {
-				root = new CircDLelement<slop*>;
+				root = new CircDLelement<shared_ptr<slop>>;
 				root->setValue(massiveGuyVec.at(i));
 				root->setPrev(root);
 				root->setNext(root);
 				currentSloppy = root;
 			} else {
-				currentSloppy->setNext(new CircDLelement<slop*>);
+				currentSloppy->setNext(new CircDLelement<shared_ptr<slop>>);
 				currentSloppy->getNext()->setPrev(currentSloppy);
 				currentSloppy = currentSloppy->getNext();
 				currentSloppy->setValue(massiveGuyVec.at(i));
@@ -111,6 +129,7 @@ class Sphere{//circular linked list and its info
 			}
 		}
 		currentSloppy = root;	
+		visualizer(bridges, currentSloppy);
 		//TODO: initialize the stuff
 	}
 	void deleteSlop(){
@@ -121,23 +140,26 @@ class Sphere{//circular linked list and its info
 			root = nullptr;
 			return;
 		}
-		CircDLelement<slop*> *nextSlop = currentSloppy->getNext();
-		CircDLelement<slop*> *prevSlop = currentSloppy->getPrev();
+		CircDLelement<shared_ptr<slop>> *nextSlop = currentSloppy->getNext();
+		CircDLelement<shared_ptr<slop>> *prevSlop = currentSloppy->getPrev();
 		prevSlop->setNext(nextSlop);
 		nextSlop->setPrev(prevSlop);
 		delete currentSloppy;
 		currentSloppy = nextSlop;
+		size--;
+		visualizer(bridges, currentSloppy);
 	}
 	void nextTurn(){
 		currentSloppy = currentSloppy->getNext();
+		visualizer(bridges, currentSloppy);
 	}
-	slop* getSlop (){
+	shared_ptr<slop> getSlop (){
 		return (currentSloppy->getValue());
 	}
 	~Sphere(){
-		CircDLelement<slop*> *curNode = currentSloppy;
-		CircDLelement<slop*> *nextNode;
-		CircDLelement<slop*> *prevNode;
+		CircDLelement<shared_ptr<slop>> *curNode = currentSloppy;
+		CircDLelement<shared_ptr<slop>> *nextNode;
+		CircDLelement<shared_ptr<slop>> *prevNode;
 		while(curNode){
 			if(curNode->getNext() == curNode){
 				delete curNode;
