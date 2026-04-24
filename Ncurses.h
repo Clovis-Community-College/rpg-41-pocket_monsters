b761adada05c8e90680bfcb3d5222370b407fbb3
deel_1003457 <1003457@my.scccd.edu>
2026-04-24 00:39:50 -0700

    The map and it's border is FINALLY DONE. im so done with this bruh. now I gotta work on actually filling in the map with stuff and accounting for the player's movement

--- Ncurses.h
+++ Ncurses.h
@@ -1,14 +1,8 @@
 #include "/public/read.h" // IWYU pragma: keep
 #include <vector>         // IWYU pragma: keep
 #include <ncurses.h>
+#include <clocale>
 using namespace std;
-
-const int MAP_HEIGHT = 30;
-const int MAP_WIDTH = 80;
-const int VIEWER_LINES = 16;
-const int VIEWER_COLS = 41;
-int MAX_X{};
-int MAX_Y{};
 
 //Q's advice (below)
 //renderworld and renderplay 
@@ -21,76 +15,68 @@ int MAX_Y{};
 //2. Update Logic (move the player x , y)
 //3. Render(draw the map) *similar to what Q said
 //4. Refresh (making ncurses showing the changes you made)
-void worldGeneration() {
-	//TODO:
-	/*Create a vector<string> or vector<vector<char>>
-	and make a nested for loop that generates the 
-	map and its size*/
-}
 
-int XPosition() {
-	//TODO:
-	//after map generation, return the 
-	//x position of the character 
-	//in relation from the bigger map_window
-	//so that you can get it in terms of 
-	//just x instead of a char
-}
-
-int YPosition() {
-	//TODO:
-	//same thing as above
-	//but for y position
-}
+const int MAP_HEIGHT = 40;
+const int MAP_WIDTH = 90;
+const int VIEWER_LINES = 26;
+const int VIEWER_COLS = 53;
+const char EMPTY = '.';
+int MAX_X{};
+int MAX_Y{};
 
 void create_map() {
 	initscr();
 	nl();
 	cbreak();
 	noecho();
-	WINDOW * map_pad{};
-	getmaxyx(map_pad, MAX_Y, MAX_X);
-	map_pad = newpad(VIEWER_LINES, VIEWER_COLS); 
-	move(MAX_Y, MAX_X);
-	refresh();
 
-	for (int i = 0; i < MAP_WIDTH; i++) {
-		for (int j = 0; j < MAP_HEIGHT; j++) {
-			// waddch (map_pad, finish the rest of this for map generation)
-		}
+	WINDOW * map{};
+	WINDOW * map_pad{};
+	getmaxyx(stdscr, MAX_Y, MAX_X);
+	int start_y = (MAX_Y - VIEWER_LINES) / 2;
+	int start_x = (MAX_X - VIEWER_COLS) / 2;
+	map = newwin(VIEWER_LINES + 2, VIEWER_COLS + 2, start_y, start_x);
+	map_pad = newpad(MAP_HEIGHT, MAP_WIDTH); 
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			mvwaddch(map_pad, i, j, EMPTY);
+			}
 	}
-	int cur_y = MAX_Y/2;
-	int cur_x = MAX_X/2; 
-	int leftcol = '|';
-	int rightcol = '|';
-	int toprow = '-';
-	int btmrow = '-';
-	int tlc = '=';
-	int trc = '=';
-	int blc = '=';
-	int brc = '=';
-	wborder(map_pad, leftcol, rightcol, toprow, btmrow, tlc, trc, blc, brc);
-	mvwprintw(map_pad, VIEWER_LINES/2, VIEWER_COLS/2, "Welcome to the Game");
-	wrefresh(map_pad);
-	int ch = getch();
-	//take input for arrow keys
+	int player_posy = VIEWER_LINES/2; 
+	int player_posx = VIEWER_COLS/2; 
+	int ch{};
 	while (ch != 'q') {
 		if (ch == KEY_UP) {
-			if (cur_y > 0) cur_y--;
-			break;
+			if (player_posy > 0) player_posy--;
 		}
 		if (ch == KEY_DOWN) {
-			if (cur_y > 0) cur_y++;
-			break;
+			if (player_posy < (MAP_HEIGHT - 1)) player_posy++;
 		}
 		if (ch == KEY_LEFT) {
-			if (cur_x > 0) cur_x--;
-		    break;
+			if (player_posx > 0) player_posx--;
 		}
 		if (ch == KEY_RIGHT) {
-			if (cur_x > 0) cur_x++;
-			break;
+			if (player_posx < (MAP_WIDTH - 1)) player_posx++;
 		}
+		int exception_case = player_posx - (VIEWER_COLS/2);
+		int exception_case$ = player_posy - (VIEWER_COLS/2);
+		if (exception_case < 0) {
+			exception_case = 0;
+		}
+		if (exception_case > (MAP_WIDTH - VIEWER_COLS)) {
+			exception_case = MAP_WIDTH - VIEWER_COLS;
+		}
+		if (exception_case$ < 0) {
+			exception_case$ = 0;
+		}
+		if (exception_case$ > (MAP_HEIGHT - VIEWER_LINES)) {
+			exception_case$ = MAP_HEIGHT - VIEWER_LINES;
+		}
+		wborder(map, '|', '|', '-', '-', '+', '+', '+', '+');
+		wnoutrefresh(map);
+		pnoutrefresh(map_pad, exception_case$, exception_case, start_y + 1, start_x + 1, start_y + VIEWER_LINES, start_x + VIEWER_COLS);
+		doupdate();
+		ch = getch();
 	}
 	endwin();
 }
